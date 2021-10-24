2d177e41b6e92c8d44d362c1da4f1f9d5d0d9007
Alexei A <alexey.alexandrenko@gmail.com>
2021-10-24 17:45:43 +0200

    fix

--- src/main/java/com/envelopes/controller/MainController.java
+++ src/main/java/com/envelopes/controller/MainController.java
@@ -1,16 +1,24 @@
-//package com.envelopes.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class MainController {
-//
-//    @RequestMapping("/index")
-//    public String index() {
-//        return "index";
-//    }
+package com.envelopes.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+
+    @RequestMapping("/index")
+    public String envelopes(@RequestParam(name = "name", required = false,
+            defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "envelopes";
+    }
+
+
+}
 //
 ////    @GetMapping("/login")
 ////    public String login() {
--- src/main/java/com/envelopes/model/Envelope.java
+++ src/main/java/com/envelopes/model/Envelope.java
@@ -1,6 +1,8 @@
 package com.envelopes.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 
