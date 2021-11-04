416d885240ef34a176669c5f7c3fd61b7631a5e9
Alexei A <alexey.alexandrenko@gmail.com>
2021-11-04 18:47:23 +0100

    Deletes target directory

--- target/classes/migrations/examples/V0.4__hb_schema_validation_fix.sql
+++
@@ -1,4 +0,0 @@
--- Workaround for: https://hibernate.atlassian.net/browse/HHH-9861
-alter table OFFER alter column BASE_PRICE     type double precision;
-alter table OFFER alter column EXTENDED_PRICE type double precision;
-alter table OFFER alter column EXTRA_PRICE    type double precision;
